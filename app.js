e3f1a91bcee9e930e7917355c8b312929b0cae0b
AndreiBotezan <andreibotezan@gmail.com>
2023-11-18 23:50:55 +0200

    done

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
-const contactsRouter = require('./routes/api/contactsAPI')
+const contactsRouter = require('./routes')
 
 const app = express()
 
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+dotenv.config();
+
+const PORT = process.env.PORT || 5000; 
+const DB_URL = process.env.DB_URL;
+
+mongoose
+	.connect(DB_URL)
+	.then(() => {
+		console.log("MongoDB is running");
+		app.listen(PORT, () => {
+			console.log("Server is running. Use our API on port: " + PORT);
+		});
+	})
+	.catch((error) => console.log("error: " + error));
